cf28229d63a4e14dd0345d29db9e1ae818be28ed
student-samratojha <samratsrojha@gmail.com>
2025-10-12 17:41:22 +0530

    Initial EJS project commit

---
+++ models/userModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    image: {
+      type: String,
+      required: [true, "Image is required"],
+    },
+    city: {
+      type: String,
+      required: [true, "City is required"],
+    },
+    adharNumber: {
+      type: String,
+      required: [true, "Aadhar number is required"],
+    },
+    occupation: {
+      type: String,
+      required: [true, "Occupation is required"],
+    },
+    education: {
+      type: String,
+      required: [true, "Education is required"],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+    },
+    dob: {
+      type: Date,
+      required: [true, "Date of birth is required"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    uniqueId: { type: String, unique: true, required: true }, // ← yahan
+  },
+  { timestamps: true }
+);
+
+// Password hashing before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Password compare method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
